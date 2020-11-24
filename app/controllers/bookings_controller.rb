7beb397d565c7d79267213bf8449f8b28e29a035
Tanguy Truhe <tanguy.clarence@gmail.com>
2020-11-24 15:35:50 +0100

    controller booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,7 +7,7 @@ class BookingsController < ApplicationController
 
   def new
     @already_booked = []
-    @studio = studio.find(params[:studio_id])
+    @studio = Studio.find(params[:studio_id])
     @studio.bookings.all.each do |booked|
       @already_booked << booked.date
     end
@@ -16,10 +16,10 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
-    @studio = studio.find(@booking.studio_id)
-    @conversation = Conversation.find_by(booking_id: @booking.id)
-    @messages = Message.where(conversation_id: @conversation.id)
-    @message = Message.new(conversation_id: @conversation.id)
+    @studio = Studio.find(@booking.studio_id)
+    # @conversation = Conversation.find_by(booking_id: @booking.id)
+    # @messages = Message.where(conversation_id: @conversation.id)
+    # @message = Message.new(conversation_id: @conversation.id)
   end
 
   def update
@@ -29,34 +29,34 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @studio = studio.find(params[:booking][:studio_id])
-    if params[:booking][:date] == ""
-      @booking = Booking.new
-      redirect_to new_studio_booking_path(@studio)
-      return
-    else
-      params[:booking][:date].split(",").each do |date|
-      date.to_date
-      @bookings = []
-      @booking = Booking.new(date: date)
-      @conversation = Conversation.new
-      @booking.studio = @studio
-      @booking.user = current_user
-      @conversation.booking = @booking
-      @conversation.save
-      @booking.save
-      @bookings << @booking
-      end
-    end
-    if @booking.nil?
-      @booking = Booking.new
-      render :new
-    elsif @bookings.each(&:save)
-      redirect_to user_path(@booking.user)
-    else
-      @booking = Booking.new
-      render :new
-    end
+    # @studio = Studio.find(params[:booking][:studio_id])
+    # if params[:booking][:date] == ""
+    #   @booking = Booking.new
+    #   redirect_to new_studio_booking_path(@studio)
+    #   return
+    # else
+    #   params[:booking][:date].split(",").each do |date|
+    #   date.to_date
+    #   @bookings = []
+    #   @booking = Booking.new(date: date)
+    #   @conversation = Conversation.new
+    #   @booking.studio = @studio
+    #   @booking.user = current_user
+    #   @conversation.booking = @booking
+    #   @conversation.save
+    #   @booking.save
+    #   @bookings << @booking
+    #   end
+    # end
+    # if @booking.nil?
+    #   @booking = Booking.new
+    #   render :new
+    # elsif @bookings.each(&:save)
+    #   redirect_to user_path(@booking.user)
+    # else
+    #   @booking = Booking.new
+    #   render :new
+    # end
   end
 
   def destroy

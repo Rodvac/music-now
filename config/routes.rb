297971c2e286b6ad221f3c00c31a2c5929febdc0
Tanguy Truhe <tanguy.clarence@gmail.com>
2020-11-24 10:55:51 +0100

    routing done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,22 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :studios
+    resources :bookings
+  end
+
+  resources :studios do
+    resources :rooms
+  end
+
+  resources :rooms do
+    resources :bookings
+    resources :items
+  end
+
+  resources :bookings do
+    resources :reviews
+  end
 end

fcb254e9ef4c7b64d2be1e3743d256d54111ec82
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-11-25 13:11:12 +0100

    work in progress on search bar

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,23 @@
 class RoomsController < ApplicationController
+
+  def index
+    if params[:search]
+      @rooms = Room.all if params[:search][:address] == "" && params[:search][:capacity] == ""
+      if params[:search][:address] != "" && params[:search][:capacity] != ""
+        @rooms = Room.global_search(params[:search][:address]).global_search(params[:search][:capacity])
+      elsif params[:search][:address] == ""
+        @rooms = Room.global_search(params[:search][:capacity])
+      else
+        @rooms = Room.global_search(params[:search][:address])
+      end
+    else
+      @rooms = Room.all
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:capacity, :name)
+  end
 end

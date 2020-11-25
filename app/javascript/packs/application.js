dd09c9c05e1ac69c193304f7e2a64edbed503783
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-11-25 18:03:56 +0100

    v2 search bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,13 @@ require("@rails/activestorage").start()
 require("channels")
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { toggleClassOnSearchBar } from '../components/searchbar';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  toggleClassOnSearchBar();
 })
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

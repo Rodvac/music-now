3607fdea1e0b1145936a6693e27a9a722c49b137
Tanguy Truhe <tanguy.clarence@gmail.com>
2020-11-26 14:27:46 +0100

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -47,12 +47,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_094937) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "chatrooms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -68,16 +62,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_094937) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_items_rooms_on_item_id"
     t.index ["room_id"], name: "index_items_rooms_on_room_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string "content"
-    t.bigint "chatroom_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -124,7 +108,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_094937) do
     t.string "first_name"
     t.string "last_name"
     t.string "status"
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -134,8 +117,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_094937) do
   add_foreign_key "bookings", "users"
   add_foreign_key "items_rooms", "items"
   add_foreign_key "items_rooms", "rooms"
-  add_foreign_key "messages", "chatrooms"
-  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "bookings"
   add_foreign_key "reviews", "users"
   add_foreign_key "rooms", "studios"

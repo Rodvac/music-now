b24d50b94168f7abdb769798b03681409af01a7a
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-11-26 15:23:50 +0100

    add a review in booking id

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,16 +7,16 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @date = params[:format].to_date
     @datearray = []
-    @date = @date - 7 
-    14.times do 
+    @date = @date - 7
+    14.times do
       @datearray << @date
       @date = @date + 1
-    end 
+    end
     @date = @date - 7
     @timearray = [ "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00" ]
     @bookings = Booking.all
   end
-  
+
   def create
     @booking = Booking.new(booking_params)
     @room = Room.find(params[:room_id])
@@ -27,17 +27,17 @@ class BookingsController < ApplicationController
       redirect_to booking_path(@booking)
     else
       render :new
-    end 
-  end 
+    end
+  end
 
   def show
     @booking = Booking.find(params[:id])
+    @review = Review.new
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:date,:time)
+    params.require(:booking).permit(:date, :time)
   end
-  
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,14 @@
 class ReviewsController < ApplicationController
 
-  def new
-    @booking = Booking.find(params[:booking_id])
-    @review = Review.new
-  end
-
   def create
-    @booking = Booking.find(params[:booking_id])
     @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.user = current_user
     @review.booking = @booking
     if @review.save
       redirect_to booking_path(@booking)
     else
-      render :new
+      render "bookings/show"
     end
   end
 

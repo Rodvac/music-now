2a9d13bb46e7729389388725b9f03998c6396c9c
Antoine Goeuriot <antoine.goeuriot@gmail.com>
2020-11-27 08:53:57 +0100

    show studio and scroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,12 +16,17 @@ import { toggleClassOnSearchBar } from '../components/searchbar';
 import { dateSelect } from '../components/dateselect';
 import { timeSelect } from '../components/timeselect';
 
+ScrollReveal().reveal('.card-studio', {delay: 500});
+ScrollReveal().reveal('.card-room', {delay: 500});
+
 document.addEventListener('turbolinks:load', () => {
   initGeoloc();
   toggleClassOnSearchBar();
   dateSelect();
   timeSelect();
   initMapbox();
+  ScrollReveal();
+  ScrollReveal().reveal();
 })
 
 

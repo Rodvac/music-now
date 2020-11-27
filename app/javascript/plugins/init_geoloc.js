fbebd924146fbe300a6ef60dabfb49d413c039c3
Delphine Habert <delphine.habert@gmail.com>
2020-11-27 13:20:25 +0100

    design index studios + JS

--- app/javascript/plugins/init_geoloc.js
+++ app/javascript/plugins/init_geoloc.js
@@ -8,6 +8,15 @@ const initGeoloc = () => {
       window.location.replace(`/studios?location[longitude]=${data.coords.longitude}&location[latitude]=${data.coords.latitude}&location[radius]=5`)
     })
   })
+  const buttonNavbar = document.querySelector("#navbar-btn")
+  if (!buttonNavbar) return;
+
+  buttonNavbar.addEventListener("click", (event) => {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition((data) => {
+      window.location.replace(`/studios?location[longitude]=${data.coords.longitude}&location[latitude]=${data.coords.latitude}&location[radius]=100`)
+    })
+  })
 }
 
 export {initGeoloc}

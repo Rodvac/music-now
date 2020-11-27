d3c9fd002ec5d5d6728df7cdcc2d3b505200aeee
Tanguy Truhe <tanguy.clarence@gmail.com>
2020-11-27 13:14:30 +0100

    yo

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  # skip_after_action :verify_authorized
+
+  def show
+    @user = User.find(params[:id])
+    @bookings = Booking.where(user_id: @user.id)
+    @studios = Studio.where(user_id: @user.id)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :status)
+  end
+end

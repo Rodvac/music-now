014fe2fd1a6b1966b8202e2afaa9a0c9a0a5b102
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-12-01 15:02:40 +0100

    add tags

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,20 +1,19 @@
 class RoomsController < ApplicationController
 
   def index
-    @rooms = Room.all
+    @rooms = Room.all.order(capacity: :asc)
     if params[:search]
       if params[:search][:address].present?
         @studios = Studio.near(params[:search][:address], 10).map(&:id)
-        @rooms = @rooms.where(studio_id: @studios).order(capacity: :asc)
+        @rooms = @rooms.where(studio_id: @studios)
       end
       if params[:search][:capacity].present?
-        @rooms = @rooms.where("capacity >= #{params[:search][:capacity]}").order(capacity: :asc)
+        @rooms = @rooms.where("capacity >= #{params[:search][:capacity]}")
       end
       if params[:search][:item].present?
-        @rooms = @rooms.global_search(params[:search][:item]).order(capacity: :asc)
+        @rooms = @rooms.global_search(params[:search][:item])
       end
-      #raise
-      @tags = params[:search].values
+      @tags = params[:search].values.select(&:present?)
     end
   end
 

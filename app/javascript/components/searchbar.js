d18e338ad11cbff64d0983b1725e58e0c74554e1
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-12-01 14:16:37 +0100

    hover on search bar

--- app/javascript/components/searchbar.js
+++ app/javascript/components/searchbar.js
@@ -1,15 +1,15 @@
-const toggleClassOnSearchBar = () => {
-  if (document.querySelector("form.search")) {
-    const searchInputs = document.querySelectorAll(".search-hover");
-    searchInputs.forEach((input) => {
-      input.addEventListener("mouseover",(event) => {
-        event.currentTarget.classList.toggle("focused")
-      });
-      input.addEventListener("mouseout",(event) => {
-        event.currentTarget.classList.toggle("focused")
-      })
-    })
-  }
-}
+// const toggleClassOnSearchBar = () => {
+//   if (document.querySelector("form.search")) {
+//     const searchInputs = document.querySelectorAll(".search-hover");
+//     searchInputs.forEach((input) => {
+//       input.addEventListener("mouseover",(event) => {
+//         event.currentTarget.classList.toggle("focused")
+//       });
+//       input.addEventListener("mouseout",(event) => {
+//         event.currentTarget.classList.toggle("focused")
+//       })
+//     })
+//   }
+// }
 
-export { toggleClassOnSearchBar } ;
+// export { toggleClassOnSearchBar } ;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ import "bootstrap";
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initGeoloc } from '../plugins/init_geoloc'
-import { toggleClassOnSearchBar } from '../components/searchbar';
+// import { toggleClassOnSearchBar } from '../components/searchbar';
 import { dateSelect } from '../components/dateselect';
 import { timeSelect } from '../components/timeselect';
 import { displayGraduation } from '../components/graduation';
@@ -24,7 +24,7 @@ ScrollReveal().reveal('.card-room', {delay: 500});
 
 document.addEventListener('turbolinks:load', () => {
   initGeoloc();
-  toggleClassOnSearchBar();
+  // toggleClassOnSearchBar();
   dateSelect();
   timeSelect();
   initMapbox();
